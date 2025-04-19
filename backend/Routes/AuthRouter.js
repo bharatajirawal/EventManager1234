875209ad29cb2615b58bb780dd12fa0a0e79abc6
Bharat Agrawal <116085675+bharatajirawal@users.noreply.github.com>
2025-04-19 22:07:49 +0530

    commit

--- backend/Routes/AuthRouter.js
+++ backend/Routes/AuthRouter.js
@@ -1,5 +1,5 @@
 const { signup, login } = require('../Controllers/AuthController');
-const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
+const { signupValidation, loginValidation } = require('../Middlewares/Authvalidation');
 
 const router = require('express').Router();
 
